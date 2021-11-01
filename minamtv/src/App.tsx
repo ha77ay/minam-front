b5b80074bbd9e0b1979c0c0b2030c9ea467f531a
Lee Seungmin <gitseungmin@gmail.com>
2021-11-01 13:20:58 +0900

    admin setup

--- minamtv/src/App.tsx
+++ minamtv/src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // main container
+    <div style={{ width: "500px", margin: "0 auto" }}>
+      <Header />
+      <div className="card" style={{ width: "18rem" }}>
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ minamtv/src/components/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <h1 style={{ color: "blue" }}>미남TV</h1>
+};
+
+export default Header;

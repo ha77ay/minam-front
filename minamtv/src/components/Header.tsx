a9e1f1f3b58fc8fa7c531a3db2ba35e67931a25e
Lee Seungmin <gitseungmin@gmail.com>
2021-11-02 09:52:10 +0900

    test

--- minamtv/src/components/Header.tsx
+++
@@ -1,5 +0,0 @@
-const Header = () => {
-  return <h1 style={{ color: "blue" }}>미남TV</h1>
-};
-
-export default Header;
---
+++ sample/pages/index.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const App = () => {
+  return (
+    <div>
+      <h2>Link to 'potato' Page</h2>
+      <Link href="/vegetable/potato">
+        <a>Move to '/vegetable/potato'</a>
+      </Link>
+    </div>
+  );
+};
+
+export default App;
